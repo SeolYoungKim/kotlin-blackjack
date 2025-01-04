c7293fc9ee97bd70bd0ef23e66a104478d01fcd4
SeolYoungKim <nasur4da@gmail.com>
2025-01-04 14:25:00 +0900

    feat: 겜블러가 베팅을 할 수 있도록 변경

--- src/main/kotlin/blackjack/domain/Gambler.kt
+++ src/main/kotlin/blackjack/domain/Gambler.kt
@@ -1,12 +1,19 @@
 package blackjack.domain
 
 class Gambler(name: String) : Participant(name) {
+    var betAmount: Int = 0
+        private set
+
     override fun isDealer(): Boolean {
         return false
     }
 
     override fun canNotReceiveCard(): Boolean {
         return score >= BlackjackRule.BLACKJACK_SCORE
+    }
+
+    fun placeBet(betAmount: Int) {
+        this.betAmount = betAmount
     }
 
     fun determineResult(dealer: Dealer): GamblerResult {
